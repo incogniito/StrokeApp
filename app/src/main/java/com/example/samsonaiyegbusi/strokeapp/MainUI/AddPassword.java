f31dbf13e3aeff0223c9826605dc44b599264360
Pen05 <penelope.roberts2012@my.ntu.ac.uk>
2016-02-19 20:34:18 +0000

    Can add a password

--- app/src/main/java/com/example/samsonaiyegbusi/strokeapp/MainUI/AddPassword.java
+++ app/src/main/java/com/example/samsonaiyegbusi/strokeapp/MainUI/AddPassword.java
@@ -1,9 +1,12 @@
 package com.example.samsonaiyegbusi.strokeapp.MainUI;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -24,9 +27,11 @@ import java.util.List;
 public class AddPassword extends AppCompatActivity {
     Context C;
     SQLiteDatabase dbHelper;
+    DatabaseHelper dbHelp;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        dbHelp = DatabaseHelper.getInstance(this);
         List questions = new ArrayList();
 
         BufferedReader reader = null;
@@ -59,26 +64,43 @@ public class AddPassword extends AppCompatActivity {
         quesSpinner.setAdapter(adaptSpinner);
 
 
-
-
-
         goButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
+                //Find what they put
                 EditText userName = (EditText)findViewById(R.id.userNametyped);
                 String userNameInput = userName.getText().toString();
 
+                //Given password
                 EditText password = (EditText)findViewById(R.id.passwordId);
                 String passwordInput = password.getText().toString();
 
+                //Given ques. ans
                 EditText quesAns = (EditText)findViewById(R.id.userAns);
                 //GET ANSWER AND CHOSEN QUESTION
                 String answer = quesAns.getText().toString();
+                //given question
                 String selected = String.valueOf(quesSpinner.getSelectedItem());
 
-                Intent goBack = new Intent(AddPassword.this, MainActivity.class);
-                startActivity(goBack);
+                dbHelp.insertIntoPasswordTable(userNameInput, passwordInput, selected, answer);
+
+
+                AlertDialog.Builder passAdded = new AlertDialog.Builder(AddPassword.this);
+                passAdded.setMessage("Password Added");
+                passAdded.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        dialog.dismiss();
+                        Intent goBack = new Intent(AddPassword.this, MainActivity.class);
+                        startActivity(goBack);
+
+                    }
+                });
+                passAdded.show();
+
+
             }
         });
     }
